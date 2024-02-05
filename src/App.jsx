5b0d78ffc023d99c45135d496834a490f4632521
Franco <lmassa.fms@gmail.com>
2024-02-05 12:38:21 -0300

    agregamos el maquetado del componente App

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ function App() {
 // aqui agregamos la logica del componente
   return (
     // aqui puedo agregar un poco mas de logica
-    <h1 className='text-center display-3'>
+    <section className='container my-4'>
+      <h1 className='text-center display-3'>
       {/* aqui agrego todo el maquetado del componente */}
       Prueba</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, laudantium nisi. Ea architecto rem sint recusandae inventore eum perferendis ut.</p>
+    </section>
   )
 }
 
