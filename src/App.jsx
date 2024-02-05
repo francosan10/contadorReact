e28d5ef6946555c3d0567d740b8aac92d2db9542
Franco <lmassa.fms@gmail.com>
2024-02-05 12:49:59 -0300

    agregamos titulo secundario

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import TituloSecundario from "./components/TituloSecundario";
 
 function App() {
-// aqui agregamos la logica del componente
+  // aqui agregamos la logica del componente
   return (
     // aqui puedo agregar un poco mas de logica
-    <section className='container my-4'>
-      <h1 className='text-center display-3'>
-      {/* aqui agrego todo el maquetado del componente */}
-      Prueba</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, laudantium nisi. Ea architecto rem sint recusandae inventore eum perferendis ut.</p>
+    <section className="container my-4">
+      <h1 className="text-center display-3">
+        {/* aqui agrego todo el maquetado del componente */}
+        Contador
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum,
+        laudantium nisi. Ea architecto rem sint recusandae inventore eum
+        perferendis ut.
+      </p>
+      <TituloSecundario></TituloSecundario>
+      {/* <TituloSecundario/> */}
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
